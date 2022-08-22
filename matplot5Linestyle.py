3618a0c6f9ad21a4bac836a6d49b63ffc0672568
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-08-22 17:31:08 +0530

    Add files via upload

---
+++ matplot5Linestyle.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, linestyle = 'dotted')
+plt.show()
+
+plt.plot(ypoints,ls='--') # dashed line
+plt.show()
+
+# linewidth or lw -
+
+plt.plot(ypoints, linewidth = '20')
+plt.show()
+
+# multiple line 
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(x1, y1, x2, y2)
+plt.show()
